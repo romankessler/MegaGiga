fdb9501fc3fe19033b2d179e7d6372c9e75a3029
Roman Kessler <r.kessler@bst.ch>
2017-11-15 21:50:11 +0100

    nugetpackage initialized

--- _MG/_MG.CLI/Properties/AssemblyInfo.cs
+++ _MG/_MG.CLI/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("_MG.CLI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Mega-Giga CLI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Funkyhead")]
 [assembly: AssemblyProduct("_MG.CLI")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
